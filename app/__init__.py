7fe20b0341e0883ce479d82576879949e504cd68
Bernice Twili <bernicetwili0@gmail.com>
2021-04-28 23:28:26 +0300

    create database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 
 # Initializing application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
+
+# Setting up configura
+app.config.from_pyfile("config.py")
 
 from app import views
---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:moringa@localhost/pitch'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+    
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
